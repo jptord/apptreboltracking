c1462a8214e8203565a33b79b6912b0881aba453
Jorge Tordoya <keizerberos@hotmail.com>
2024-04-21 20:47:53 -0400

    first commit

---
+++ app/src/main/java/com/kernotec/apptreboltrackingv2/MainActivity.java
@@ -0,0 +1,172 @@
+package com.kernotec.apptreboltrackingv2;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import org.osmdroid.events.ScrollEvent;
+import org.osmdroid.events.ZoomEvent;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+
+import java.util.Map;
+import egolabsapps.basicodemine.offlinemap.Interfaces.GeoPointListener;
+import egolabsapps.basicodemine.offlinemap.Interfaces.MapListener;
+import egolabsapps.basicodemine.offlinemap.Utils.MapUtils;
+import egolabsapps.basicodemine.offlinemap.Views.OfflineMapView;
+
+public class MainActivity extends AppCompatActivity implements MapListener, GeoPointListener {
+
+    OfflineMapView offlineMapView;
+    MapUtils mapUtils;
+    private static final int STORAGE_PERMISSION_CODE = 23;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        offlineMapView = findViewById(R.id.map);
+        offlineMapView.init(this, this);
+
+        if (!checkStoragePermissions())
+            requestForStoragePermissions();
+    }
+    public boolean checkStoragePermissions(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            //Android is 11 (R) or above
+            return Environment.isExternalStorageManager();
+        }else {
+            //Below android 11
+            int write = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            int read = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+            return read == PackageManager.PERMISSION_GRANTED && write == PackageManager.PERMISSION_GRANTED;
+        }
+    }
+    private void requestForStoragePermissions() {
+        //Android is 11 (R) or above
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            try {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                Uri uri = Uri.fromParts("package", this.getPackageName(), null);
+                intent.setData(uri);
+                storageActivityResultLauncher.launch(intent);
+            }catch (Exception e){
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                storageActivityResultLauncher.launch(intent);
+            }
+        }else{
+            //Below android 11
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                    },
+                    STORAGE_PERMISSION_CODE
+            );
+        }
+
+    }
+
+    private ActivityResultLauncher<Intent> storageActivityResultLauncher =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>(){
+
+                        @Override
+                        public void onActivityResult(ActivityResult o) {
+                            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+                                //Android is 11 (R) or above
+                                if(Environment.isExternalStorageManager()){
+                                    //Manage External Storage Permissions Granted
+                                    Log.d(TAG, "onActivityResult: Manage External Storage Permissions Granted");
+                                }else{
+                                    Toast.makeText(MainActivity.this, "Storage Permissions Denied", Toast.LENGTH_SHORT).show();
+                                }
+                            }else{
+                                //Below android 11
+
+                            }
+                        }
+                    });
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == STORAGE_PERMISSION_CODE){
+            if(grantResults.length > 0){
+                boolean write = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean read = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                if(read && write){
+                    Toast.makeText(MainActivity.this, "Storage Permissions Granted", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Storage Permissions Denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void mapLoadSuccess(MapView mapView, MapUtils mapUtils) {
+
+        this.mapUtils = mapUtils;
+        offlineMapView.setInitialPositionAndZoom(new GeoPoint(-16.489689, -68.119293), 14.5);
+        mapView.setMinZoomLevel(12.0);
+        mapView.setMaxZoomLevel(24.0);
+
+        Marker marker = new Marker(mapView);
+        marker.setPosition(new GeoPoint(-16.489689, -68.119293));
+        marker.setIcon(getResources().getDrawable(R.drawable.galata_tower));
+
+        // marker.setImage(drawable);
+
+        marker.setTitle("Hello Istanbul");
+        marker.showInfoWindow();
+        mapView.getOverlays().add(marker);
+        mapView.invalidate();
+    }
+
+    @Override
+    public void mapLoadFailed(String ex) {
+
+        Log.e("ex:", ex);
+    }
+
+    @Override
+    public void onGeoPointRecieved(GeoPoint geoPoint) {
+
+
+        Toast.makeText(this, geoPoint.toDoubleString(), Toast.LENGTH_SHORT).show();
+    }
+
+    public void activateAnimatePicker(View view) {
+        if (mapUtils != null)
+            offlineMapView.setAnimatedLocationPicker(true, this, mapUtils);
+    }
+
+}
